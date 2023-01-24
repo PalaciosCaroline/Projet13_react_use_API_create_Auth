b3dbc4c5c241cc53a95d0cfd4ba6e9d719a36e99
PalaciosCaroline <caroline2807@yahoo.fr>
2023-01-24 09:31:13 +0100

    installation de redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
+import { store } from "./store";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
