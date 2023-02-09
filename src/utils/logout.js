5dae233d58f86aeca5b7ecdc4a931b6e4df68832
PalaciosCaroline <caroline2807@yahoo.fr>
2023-02-09 09:50:38 +0100

    logout: commentaire

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -3,6 +3,7 @@ import { redirect } from 'react-router-dom';
 import { useDispatch , useSelector} from 'react-redux';
 import { logout, setIsAuthentificated } from './../store/user.slice';
 
+//pour l'instant non utiliser
 export default function LogoutUser() {
 //   const history = useHistory();
   const dispatch = useDispatch();
