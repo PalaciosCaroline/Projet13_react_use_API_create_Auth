29ced37bce4ca9942f20dc06c58193e44dbd2ee2
PalaciosCaroline <caroline2807@yahoo.fr>
2023-02-15 12:07:48 +0100

    fichier swagger

--- src/components/HeaderProfile.jsx
+++ src/components/HeaderProfile.jsx
@@ -38,31 +38,7 @@ export default function HeaderProfile() {
   //   event.target.value = '';
   // };
 
-  // const handleNameChange = (name, setName) => event => {
-  //   const input = event.target.value;
-  //   const regex = /^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ\-'\s]{0,40}$/;
-  //   if (!regex.test(input)) {
-  //     event.target.classList.add('error');
-  //     setErrorName(`Please enter a valid ${name}`);
-  //     return;
-  //   } else if (regex.test(input)) {
-  //       setName(input);
-  //       event.target.classList.remove('error');
-  //       setErrorName('');
-  //   }
-  // };
-
-  // const controlLenghtName  = (nameToControl, name) => {
-  //   if (nameToControl.length <= 1) {
-  //       setErrorName(`The ${name} must contain at least 2 letters`);
-  //       document.getElementById(`input_${name}`).classList.add('error');
-  //     return false;
-  //   } else {
-  //     setErrorName('');
-  //     document.getElementById(`input_${name}`).classList.remove('error');
-  //     return true;
-  //   }
-  // }
+  
 
   const handleFirstNameChange = handleNameChange(setErrorName, 'firstname', setFirstName);
   const handleLastNameChange = handleNameChange(setErrorName, 'lastname', setLastName);
